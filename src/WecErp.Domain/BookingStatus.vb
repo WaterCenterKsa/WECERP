9aa5e9808af2012d7660f5f276d061741edd57d7
WaterCenterKsa <watercenterksa@gmail.com>
2026-09-18 00:39:21 +0300

    Add booking status model

---
+++ src/WecErp.Domain/BookingStatus.vb
@@ -0,0 +1,9 @@
+Namespace WecErp.Domain
+    Public Enum BookingStatus
+        Draft = 1
+        Confirmed = 2
+        Completed = 3
+        Cancelled = 4
+        NoShow = 5
+    End Enum
+End Namespace
