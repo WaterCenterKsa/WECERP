3bd0ca2439c866365a19343b448e95860c4af2d9
WaterCenterKsa <watercenterksa@gmail.com>
2026-09-17 23:30:33 +0300

    Add quotation status domain model

---
+++ src/WecErp.Domain/QuotationStatus.vb
@@ -0,0 +1,10 @@
+Namespace WecErp.Domain
+    Public Enum QuotationStatus
+        Draft = 1
+        Sent = 2
+        Accepted = 3
+        Rejected = 4
+        Expired = 5
+        Cancelled = 6
+    End Enum
+End Namespace
