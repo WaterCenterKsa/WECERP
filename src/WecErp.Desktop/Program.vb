b62de84fd31d1b621377cddf451ae920dcf3032e
WaterCenterKsa <watercenterksa@gmail.com>
2026-09-17 23:25:14 +0300

    Fix VB WinForms startup configuration

--- src/WecErp.Desktop/Program.vb
+++ src/WecErp.Desktop/Program.vb
@@ -4,7 +4,9 @@ Imports System.Windows.Forms
 Module Program
     <STAThread>
     Sub Main()
-        ApplicationConfiguration.Initialize()
+        Application.SetHighDpiMode(HighDpiMode.SystemAware)
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New MainForm())
     End Sub
 End Module
