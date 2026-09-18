571f646bb567431ef6e1ad5a18e27e88ec96ab36
WaterCenterKsa <watercenterksa@gmail.com>
2026-09-18 04:45:05 +0300

    Add EF Core design-time DbContext factory

---
+++ src/WecErp.Infrastructure/ErpDbContextFactory.vb
@@ -0,0 +1,22 @@
+Imports System
+Imports Microsoft.EntityFrameworkCore
+Imports Microsoft.EntityFrameworkCore.Design
+
+Namespace WecErp.Infrastructure
+    Public Class ErpDbContextFactory
+        Implements IDesignTimeDbContextFactory(Of ErpDbContext)
+
+        Public Function CreateDbContext(args As String()) As ErpDbContext Implements IDesignTimeDbContextFactory(Of ErpDbContext).CreateDbContext
+            Dim connectionString = Environment.GetEnvironmentVariable("WECERP_DESIGN_CONNECTION")
+            If String.IsNullOrWhiteSpace(connectionString) Then
+                connectionString = "Server=(localdb)\MSSQLLocalDB;Database=WecErp;Trusted_Connection=True;TrustServerCertificate=True"
+            End If
+
+            Dim options = New DbContextOptionsBuilder(Of ErpDbContext)().
+                UseSqlServer(connectionString).
+                Options
+
+            Return New ErpDbContext(options)
+        End Function
+    End Class
+End Namespace
